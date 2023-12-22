c38448697cb0cdefb2f8f2da8fe8c147649e7d90
Nicolas Gariepy <ngariepy1999@gmail.com>
2023-12-22 15:15:23 -0500

    cleanup data

--- serial-analyzer/__main__.py
+++ serial-analyzer/__main__.py
@@ -1,10 +1,11 @@
 import serial
 
 if __name__ == "__main__":
+    print("Start")
     while 1:
         try:
-            with serial.Serial('/dev/ttyACM1', 115200, timeout=1) as ser:
+            with serial.Serial('/dev/ttyACM1', 115200) as ser:
                 x = ser.read()
-                print(x)
+                print(int.from_bytes(x))
         except serial.serialutil.SerialException:
             pass
